cd01bcb1923de6abea04872281a313b941040d0a
SinySs <dimakrytoi8@gmail.com>
2022-05-03 22:32:01 +0300

    Add a small description. Changed the design of the code

--- Doubly_connected_list.h
+++ Doubly_connected_list.h
@@ -1,21 +1,22 @@
+/*!
+*This file contains a doubly linked list and functions for working with it.
+*/
+
 #ifndef DOUBLY_CONNECTED_LIST_H_INCLUDED
 #define DOUBLY_CONNECTED_LIST_H_INCLUDED
 
 struct list_elem {
+  int data;
 
-    int data;
-
-    struct list_elem *next;
-    struct list_elem *prev;
-
+  struct list_elem *next;
+  struct list_elem *prev;
 };
 
 struct List {
+  struct list_elem *head;
+  struct list_elem *tail;
 
-    struct list_elem *head;
-    struct list_elem *tail;
-
-    int size;
+  int size;
 };
 
 int create_list(struct List *list);
@@ -28,9 +29,10 @@ struct list_elem *push_head(struct List *list, int data);
 
 struct list_elem *push_tail(struct List *list, int data);
 
-struct list_elem *push_after(struct List *list, struct list_elem *elem, int data);
+struct list_elem *push_after(struct List *list, struct list_elem *elem,
+                             int data);
 
-struct list_elem *push_before(struct List *list, struct list_elem *elem, int data);
-
+struct list_elem *push_before(struct List *list, struct list_elem *elem,
+                              int data);
 
 #endif // DOUBLY_CONNECTED_LIST_H_INCLUDED
