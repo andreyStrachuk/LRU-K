5622fa8705e6390f7fbcbfd0e7dc4c1888baf4cb
Ксения Добровольская <kseniadmitrievnadobrovolskaya@mail.ru>
2022-05-11 17:08:48 +0300

    replacing all tabs with two spaces

--- src/main.c
+++ src/main.c
@@ -13,50 +13,49 @@ int read_number(int *number);
 
 int main()
 {
-	int scan, len_cache, len_hash_table, number_pages, page, hits;
-	struct List cache;
-	struct Hash hash_table;
-	hits = 0;
+  int scan, len_cache, len_hash_table, number_pages, page, hits;
+  struct List cache;
+  struct Hash hash_table;
+  hits = 0;
 
-	read_number(&len_cache);
+  read_number(&len_cache);
 
-	read_number(&number_pages);
+  read_number(&number_pages);
 
-	//cache = create_cache(len_cache);
+  //cache = create_cache(len_cache);
 
-	len_hash_table = relative_hash_table_size * number_pages;
-	//hash_table = create_hash_table(len_hash_table);
+  len_hash_table = relative_hash_table_size * number_pages;
+  //hash_table = create_hash_table(len_hash_table);
 
 
-	for (int count_pages = 0; count_pages < number_pages; count_pages++)
-	{
-		read_number(&page);
+  for (int count_pages = 0; count_pages < number_pages; count_pages++)
+  {
+    read_number(&page);
+    cache = append_page(page, hash_table, cache, &hits);
 
-		//cache = append_page(page, hash_table, cache, &hits);
+  }
 
-	}
+  printf("%d\n", hits);
 
-	printf("%d\n", hits);
-
-	return 0;
+  return 0;
 
 }
 
 int read_number(int *number)
 {
-	int scan;
+  int scan;
 
-	scan = scanf("%d", number);
+  scan = scanf("%d", number);
 
-	assert(scan && "Input Error");
+  assert(scan && "Input Error");
 
-	if ((*number) < 0)
-	{
-		scan = 0;
-		assert(scan && "Wrong format");
-	}
+  if ((*number) < 0)
+  {
+    scan = 0;
+    assert(scan && "Wrong format");
+  }
 
-	return 0;
+  return 0;
 }
 
 
